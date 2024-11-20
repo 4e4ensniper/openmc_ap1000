e8307f1347f6c6906a0ac0e4a05cdfedbbc825e9
Kirill Efanov <kir.efanoff@yande.ru>
2024-11-20 21:45:16 +0300

    3 more axes of symmetry are taken into account (6 in total)

--- constants.py
+++ constants.py
@@ -10,12 +10,25 @@ core_barrel_in_r = 339.72/2 #sm
 core_barrel_out_r = 349.88/2 #sm
 #The number of fuel assemblies per row, starting from the bottom of the core map.
 line = [4, 7, 10, 11, 12, 13, 12, 13, 12, 13, 12, 11, 10, 7, 4]
+#Path to the location of files with temperature distributions.
 csv_path = "/home/ubuntu24/Desktop/openmc_ap1000/materials/temperature_distributions/"
+#Calculation parameters
 batches = 200
 inactive = 10
 particles = 10000
-n_dif = 26
+#Number of different fuel assemblies defined as different universes
+n_dif = 18
+#An array that contains the numbers of the various universes of fuel assemblies on the cartogram.
+half_numbers = [1, 2, 2, 1,
+                3, 4, 5, 6, 5, 4, 3,
+                1, 4, 7, 8, 9, 9, 8, 7, 4, 1,
+                2, 5, 8, 10, 11, 12, 11, 10, 8, 5, 2,
+                2, 6, 9, 11, 13, 14, 14, 13, 11, 9, 6, 2,
+                1, 5, 9, 12, 14, 15, 16, 15, 14, 12, 9, 5, 1,
+                4, 8, 11, 14, 16, 17, 17, 16, 14, 11, 8, 4,
+                3, 7, 10, 13, 15, 17]
 
+'''
 half_numbers = [1,  2,  3,  4,
            5,  6,  7,  8,  9, 10,  5,
            4, 10, 11, 12, 13, 14, 15, 11,  6,  1,
@@ -24,6 +37,7 @@ half_numbers = [1,  2,  3,  4,
            1,  7, 13, 18, 22, 23, 24, 23, 21, 18, 14,  9, 4,
            6, 12, 17, 21, 24, 25, 25, 24, 22, 19, 15, 10,
            5, 11, 16, 20, 23, 25] #Half of the fuel assemblies number on the cartogram without the central cassette
+'''
 r_half_numbers = list(reversed(half_numbers))
 half_numbers.append(n_dif)
 numbers = half_numbers + r_half_numbers
